1a83885317b5583a17613518c5d87fa338df8a9f
josephlin8909 <josephlin89@gmail.com>
2023-03-21 04:47:11 +0800

    added rocket

---
+++ src/rocket.rs
@@ -0,0 +1,32 @@
+#[macro_use]
+extern crate rocket;
+
+#[cfg(test)]
+mod tests;
+
+#[derive(FromFormField)]
+enum Lang {
+    #[field(value = "en")]
+    English,
+    #[field(value = "ru")]
+    #[field(value = "ру")]
+    Russian,
+}
+
+#[derive(FromForm)]
+struct Options<'r> {
+    emoji: bool,
+    name: Option<&'r str>,
+}
+
+// Try visiting:
+//   http://127.0.0.1:8000/hello/world
+#[get("/")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![world])
+}
