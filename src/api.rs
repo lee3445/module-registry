da34c968873ae585c25e01ce8d707b0ac88aee94
lee3445 <70142501+lee3445@users.noreply.github.com>
2023-03-20 22:28:50 -0400

    Remove unused struct
    Signed-off-by: lee3445 <70142501+lee3445@users.noreply.github.com>

--- src/api.rs
+++ src/api.rs
@@ -1,23 +1,8 @@
 //#[cfg(test)]
 //mod tests;
 
-#[derive(FromFormField)]
-enum Lang {
-    #[field(value = "en")]
-    English,
-    #[field(value = "ru")]
-    #[field(value = "ру")]
-    Russian,
-}
-
-#[derive(FromForm)]
-struct Options<'r> {
-    emoji: bool,
-    name: Option<&'r str>,
-}
-
 // Try visiting:
-//   http://127.0.0.1:8000/hello/world
+//   http://127.0.0.1:8000/
 #[get("/")]
 pub fn world() -> &'static str {
     "Hello, world!"
