3ada983027c64c9b09fac03902680eba66efb9ff
plohanak <plohanak@purdue.edu>
2023-03-30 23:02:09 -0400

    mount new api route

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,7 @@ use api::*;
 
 #[launch]
 async fn rocket() -> _ {
-    rocket::build().mount("/", routes![world, test, package_rate]).manage(database::module_db().await)
+    rocket::build()
+        .mount("/", routes![world, package_retrieve, package_rate])
+        .manage(database::module_db().await)
 }
