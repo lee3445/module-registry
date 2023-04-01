4b26b810433507138c63cba201d4cafbe5ac1a28
Albert Lee <lee3445@purdue.edu>
2023-04-01 14:59:32 -0400

    finally

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,13 @@
+use std::path::Path;
+use rocket::fs::{NamedFile, relative};
 //#[cfg(test)]
 //mod tests;
 
 // Try visiting:
 //   http://127.0.0.1:8000/
 #[get("/")]
-pub fn world() -> &'static str {
-    "Hello, world!\n"
+pub async fn world() -> Option<NamedFile> {
+    NamedFile::open(Path::new(relative!("index.html"))).await.ok()
 }
 #[get("/test")]
 pub fn test() -> &'static str {
