c9c0d230b06845ea64ab770ac3645b9a68d2cea3
TroyWu <trotwuu@gmail.com>
2023-04-03 02:50:42 +0000

    CD test 5

--- src/api.rs
+++ src/api.rs
@@ -1,24 +1,17 @@
-use std::path::Path;
-use rocket::fs::{NamedFile, relative};
 //#[cfg(test)]
 //mod tests;
 
 // Try visiting:
 //   http://127.0.0.1:8000/
 #[get("/")]
-<<<<<<< HEAD
 pub fn world() -> &'static str {
-    "Hello, world4!\n"
-=======
-pub async fn world() -> Option<NamedFile> {
-    NamedFile::open(Path::new(relative!("index.html"))).await.ok()
->>>>>>> 4b26b810433507138c63cba201d4cafbe5ac1a28
+    "Hello, world5!\n"
 }
 #[get("/test")]
 pub fn test() -> &'static str {
-    "Hello, test4!\n"
+    "Hello, test5!\n"
 }
 #[get("/package/<id>")]
 pub fn package(id:String) -> &'static str {
-    "Hello, package4\n"
+    "Hello, package5\n"
 }
