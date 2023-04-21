d5b821ae129f0bd25876b2c50c21b95532f9a4da
josephlin8909 <josephlin89@gmail.com>
2023-04-22 04:15:11 +0800

    update main and index.html

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ mod database;
 extern crate rocket;
 
 use api::*;
+use rocket::fairing;
+use rocket::http::Header;
+use rocket::{Request, Response};
 
 //#[cfg(test)]
 //mod tests;
 
 #[launch]
 async fn rocket() -> _ {
-
     let port: u32 = std::env::var("PORT")
         .ok()
         .and_then(|p| p.parse::<u32>().ok())
@@ -41,4 +43,25 @@ async fn rocket() -> _ {
         )
         .register("/packages", catchers![packages_list_422])
         .manage(database::module_db().await)
+        .attach(Cors)
+}
+
+struct Cors;
+#[rocket::async_trait]
+impl fairing::Fairing for Cors {
+    fn info(&self) -> fairing::Info {
+        fairing::Info {
+            name: "Add CORS headers to responses",
+            kind: fairing::Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow_Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow_Methods",
+            "GET, POST, PUT, DELETE",
+        ));
+        response.set_header(Header::new("Access-Control-Allow_Headers", "*"));
+    }
 }
