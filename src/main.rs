1e86345b0655623ad582a422c17eb17d8d8c2890
josephlin8909 <josephlin89@gmail.com>
2023-04-22 05:15:42 +0800

    update main and index.html

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,10 @@ async fn rocket() -> _ {
     let port: u32 = std::env::var("PORT")
         .ok()
         .and_then(|p| p.parse::<u32>().ok())
-        .unwrap_or(8080);
+        .unwrap_or(8000);
     let figment = rocket::Config::figment()
         .merge(("port", port))
-        .merge(("address", "0.0.0.0"));
+        .merge(("address", "127.0.0.1"));
 
     rocket::custom(figment)
         .mount(
