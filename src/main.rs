331159fee7de3f2d6a1eb2d0cb6151db268b6f62
josephlin8909 <josephlin89@gmail.com>
2023-04-25 04:44:19 +0800

    fixed CORS headers and website gui

--- src/main.rs
+++ src/main.rs
@@ -57,11 +57,11 @@ impl fairing::Fairing for Cors {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow_Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
         response.set_header(Header::new(
-            "Access-Control-Allow_Methods",
+            "Access-Control-Allow-Methods",
             "GET, POST, PUT, DELETE",
         ));
-        response.set_header(Header::new("Access-Control-Allow_Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
     }
 }
